b01a3702dbda63f7feab12fb4fb77e4a46c6c69b
Bec <84033825+becgr4y@users.noreply.github.com>
2021-12-07 08:19:02 +0000

    Much better

--- 2021/day_7.py
+++ 2021/day_7.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 with open("2021/inputs/7") as f:
     data = np.array([int(item) for line in f for item in line.strip().split(",")])
@@ -17,28 +18,20 @@ print(sum)
 
 # Part 2
 
+mean = np.mean(data)
+mean_ceil = math.ceil(mean)
+mean_floor = math.floor(mean)
+
+sum_ceil = 0
+sum_floor = 0
+
+
+def triangle(number):
+    return number * (number + 1) / 2
+
+
 for item in data:
-    distance = item
-    cost = distance * (distance + 1) / 2
-    sum += cost
-best = sum
+    sum_ceil += triangle(abs(mean_ceil - item))
+    sum_floor += triangle(abs(mean_floor - item))
 
-for position in range(1, np.max(data) + 1):
-    sum = 0
-    for item in data:
-        distance = abs(position - item)
-        cost = distance * (distance + 1) / 2
-        sum += cost
-    if sum < best:
-        best = sum
-        best_position = position
-
-print(best_position)
-print(best)
-
-# Part 2 w/ maths
-
-X = np.sum(data)
-N = len(data)
-
-print(0.5 * (2 * X - N) / N)
+print(min(sum_ceil, sum_floor))
