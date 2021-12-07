a3d27a5610e9d9cb9b93a4d1c402f34f2076170b
Bec <84033825+becgr4y@users.noreply.github.com>
2021-12-07 05:30:28 +0000

    Day 7

---
+++ 2021/day_7.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+with open("2021/inputs/7") as f:
+    data = np.array([int(item) for line in f for item in line.strip().split(",")])
+
+# Part 1
+
+median = np.round(np.median(data))
+
+sum = 0
+
+for item in data:
+    sum += abs(median - item)
+
+print(median)
+print(sum)
+
+# Part 2
+
+for item in data:
+    distance = item
+    cost = distance * (distance + 1) / 2
+    sum += cost
+best = sum
+
+for position in range(1, np.max(data) + 1):
+    sum = 0
+    for item in data:
+        distance = abs(position - item)
+        cost = distance * (distance + 1) / 2
+        sum += cost
+    if sum < best:
+        best = sum
+        best_position = position
+
+print(best_position)
+print(best)
